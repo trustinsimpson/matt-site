d638c323f339816e580f113a1e10a73d908cc70b
Trustin Simpson <contact@trustinsimpson.com>
2021-02-10 13:32:04 -0600

    added overlay to carousel images

--- src/components/carousel.js
+++ src/components/carousel.js
@@ -4,29 +4,37 @@ import BP from '../assets/blueprints2.jpg';
 import Construction from '../assets/house-with-blueprint.jpg';
 
 
-const Caro = () => {
+const Caro = (props) => {
 
   const picHeight = '885px';
 
-  const overlay = {
-    zIndex: '1',
-    height: '100%',
-    width: '100%',
-    position: 'fixed',
-    overflow: 'auto',
-    top: '0px',
-    left: '0px',
-    backgroundColor: 'rgba(0, 0, 0, 0.7)'
+  const styles = {
+    overlay: {
+      zIndex: '1',
+      height: '100%',
+      width: '100%',
+      position: 'absolute',
+      overflow: 'auto',
+      top: '0px',
+      left: '0px',
+      backgroundColor: 'rgba(0, 0, 0, 0.6)'
+    },
+    image: {
+      backgroundImage: `url(${props.link})`,
+      height: '100vh',
+      backgroundPosition: 'center',
+      backgroundRepeat: 'no-repeat',
+      backgroundSize: 'cover'
+    }
   };
 
   return (
-    <Carousel className="">
+    <Carousel>
       <Carousel.Item>
         <img
           className="d-block w-100"
           style={{ height: picHeight }}
           src={BP}
-          // src="../assets/blueprints2.jpg/800x400?text=First slide&bg=373940"
           alt="First slide"
         />
         <Carousel.Caption>
@@ -34,14 +42,15 @@ const Caro = () => {
           <p>Your dream home starts with the concept! We'll help you get it right the first time!</p>
         </Carousel.Caption>
       </Carousel.Item>
-      <Carousel.Item style={{ overlay }}>
+      <Carousel.Item>
         <img
           className="d-block w-100 tw-overscroll-none"
           style={{ height: picHeight }}
           src={Construction}
           alt="Third slide"
         />
-
+                  <div style={styles.overlay}>
+          </div>
         <Carousel.Caption>
           <h3>Revisions</h3>
           <p>Got a set of plans but not quite your dream? We can fix that!</p>
@@ -54,7 +63,8 @@ const Caro = () => {
           src={Construction}
           alt="Third slide"
         />
-
+          <div style={styles.overlay}>
+          </div>
         <Carousel.Caption>
           <h3>Third slide label</h3>
           <p>
