2927d77b3934e68bd0518400a2cdf79224bb5aff
Trustin Simpson <contact@trustinsimpson.com>
2021-02-21 23:28:52 -0600

    changed styling of header to fix bug with background image and refactored code for simplicity

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,44 +1,24 @@
 import React from "react";
-import { Button, Container, Jumbotron } from "react-bootstrap";
+import { Button, Container } from "react-bootstrap";
 import background from "../../assets/home.jpg";
 import "./header.css";
 
 const Header = () => {
-  const styles = {
-    image: {
-      backgroundImage: `url(${background})`,
-      minHeight: "55rem",
-      backgroundPosition: "center",
-      backgroundRepeat: "no-repeat",
-      backgroundSize: "cover",
-      display: "block",
-    },
-    overlay: {
-      zIndex: "0",
-      height: "100%",
-      width: "100%",
-      position: "absolute",
-      overflow: "auto",
-      top: "0px",
-      left: "0px",
-      backgroundColor: "rgba(0, 0, 0, 0.55)",
-    }
-  };
 
   return (
-    <div>
-    <div style={styles.overlay}></div>
-    <Jumbotron fluid className="lead mb-0 tw-text-white" style={styles.image}>
-      <Container fluid className="tw-absolute tw-z-10">
+    <div className="heroImage" style={{backgroundImage: `url(${background})`}}>
+    <div className="heroOverlay"></div>
+    
+      <Container fluid className="tw-relative tw-text-white tw-p-9">
         <h1>Welcome Home!</h1>
         <p className="tw-font-semibold">
-          A drafting agency that cares as much about your next home as you do.
+          MM Designs, where we care as much about your next home as you do.
         </p>
         <p>
           <Button variant="primary">Learn more</Button>
         </p>
       </Container>
-    </Jumbotron>
+    
     </div>
   );
 };
